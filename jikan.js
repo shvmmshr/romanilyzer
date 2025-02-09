e809d93ceff043ad62d58f93ba937cc773d5910c
Matthew <matthewlorenz@protonmail.com>
2025-02-10 01:21:01 +0800

    Add files via upload

---
+++ jikan.js
@@ -0,0 +1,23 @@
+const jikanData = {
+  data: {
+    mal_id: 0,
+    url: "string",
+    images: {
+      jpg: {
+        image_url: "string",
+        small_image_url: "string",
+      },
+      webp: {
+        image_url: "string",
+        small_image_url: "string",
+      },
+    },
+    name: "string",
+    name_kanji: "string",
+    nicknames: ["string"],
+    favorites: 0,
+    about: "string",
+  },
+};
+
+module.exports = jikanData;
