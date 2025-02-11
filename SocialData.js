36ee457cefd51804bd7b816db90d8ca88be2a920
Matthew <matthewlorenz@protonmail.com>
2025-02-11 14:47:18 +0800

    Add files via upload

---
+++ SocialData.js
@@ -0,0 +1,35 @@
+const userIdUrl = "https://api.socialdata.tools/twitter/user/[user_id]";
+const screenNameUrl = "https://api.socialdata.tools/twitter/user/[screen_name]";
+
+async function getSocialData() {
+  try {
+    const response = await fetch(
+      "https://api.socialdata.tools/twitter/user/elonmusk",
+      {
+        headers: {
+          Authorization:
+            "1986|9vA4xJl84iCbpJezHffZFSuiIz39qJSgXTWIpkx42ce1080f",
+          Accept: "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
+}
+fetch("https://api.socialdata.tools/twitter/user/MMatt14", {
+  headers: {
+    Authorization: "1986|9vA4xJl84iCbpJezHffZFSuiIz39qJSgXTWIpkx42ce1080f",
+    Accept: "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
